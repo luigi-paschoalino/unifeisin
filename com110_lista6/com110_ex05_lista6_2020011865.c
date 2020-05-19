b18239f9f56fa94f73b37a91d045eaad55d8827f
luigi-paschoalino <64054620+luigi-paschoalino@users.noreply.github.com>
2020-05-19 15:37:18 -0300

    Add files via upload

--- com110_lista6/com110_ex05_lista6_2020011865.c
+++ com110_lista6/com110_ex05_lista6_2020011865.c
@@ -15,12 +15,13 @@ int main()
 	scanf("%d",&chave);
 	for(i=0;i<qtdlet;i++)
 	{	
+		//Visto que cada letra possui um valor único na ASCII, precisamos garantir que ele fique dentro dos valores do alfabeto
 		if(palavra[i]+chave<97)
-			printf("%c",palavra[i]+chave+26);
+			printf("%c",palavra[i]+chave+26); //Este aqui é para quando a chave for negativa e a soma da chave e o caractere for menor que o valor da letra "a"
 		else if(palavra[i]+chave>122)
-			printf("%c",palavra[i]+chave-26);
+			printf("%c",palavra[i]+chave-26); //Este é para quando o valor for maior que a da letra "z"
 		else
-			printf("%c",(palavra[i])+chave);
+			printf("%c",(palavra[i])+chave); //Este é nos casos normais
 	}
 	return 0;
 }
